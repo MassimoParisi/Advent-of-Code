4bb5249830aa2e83a7b38be2c6ecd37a12d9e5ee
MassimoParisi <10583470@polimi.it>
2020-12-09 19:28:46 +0100

    2015 day4 problems

---
+++ 2015/Day04/problem2.py
@@ -0,0 +1,13 @@
+import hashlib
+
+with open("input.txt") as f:
+    inp = f.read()
+
+result = 0
+while True:
+    to_check = inp + str(result)
+    h = hashlib.md5(to_check.encode())
+    if h.hexdigest()[:6] == "000000":
+        print(f"Result is: {result}")
+        break
+    result += 1
