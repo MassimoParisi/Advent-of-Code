c56ef53e86ebe758e2de2e46551c80cefa838e29
MassimoParisi <10583470@polimi.it>
2020-12-10 21:22:47 +0100

    2017 day5 problems

---
+++ 2017/Day05/problem1.py
@@ -0,0 +1,16 @@
+arr = []
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        arr.append(int(line))
+
+i = 0
+result = 0
+arr_len = len(arr)
+while 0 <= i < arr_len:
+    jump = arr[i]
+    arr[i] += 1
+    i += jump
+    result += 1
+
+print(result)
