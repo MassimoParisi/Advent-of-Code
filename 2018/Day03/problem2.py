eabf78160538ab33c80057fdf21e27b2fd23e5d9
MassimoParisi <10583470@polimi.it>
2020-12-12 14:33:18 +0100

    2018 day3 problems

---
+++ 2018/Day03/problem2.py
@@ -0,0 +1,37 @@
+import re
+
+used = set()
+overlap = set()
+pattern = r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        tmp = re.match(pattern, line)
+        x0 = int(tmp.group(2))
+        y0 = int(tmp.group(3))
+        wide = int(tmp.group(4))
+        tall = int(tmp.group(5))
+        for i in range(x0, x0 + wide):
+            for j in range(y0, y0 + tall):
+                if (i, j) in used:
+                    overlap.add((i, j))
+                else:
+                    used.add((i, j))
+    f.seek(0)
+    for line in f.readlines():
+        tmp = re.match(pattern, line)
+        id = int(tmp.group(1))
+        x0 = int(tmp.group(2))
+        y0 = int(tmp.group(3))
+        wide = int(tmp.group(4))
+        tall = int(tmp.group(5))
+        found = True
+        for i in range(x0, x0 + wide):
+            for j in range(y0, y0 + tall):
+                if (i, j) in overlap:
+                    found = False
+                    break
+            if not found: break
+        if found: break
+
+    print(f"Result is: {id}")
--- README.md
+++ README.md
@@ -134,7 +134,7 @@ My AoC solutions, written in Python.
       <table>
         <td> <a href="2018/Day01">1</a> </td>
         <td> <a href="2018/Day02">2</a> </td>
-        <td>3</td>
+        <td> <a href="2018/Day03">3</td>
         <td>4</td>
         <td>5</td>
   </tr>
