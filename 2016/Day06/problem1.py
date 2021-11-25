3884b9e20e2a39dbb477092f3a42bfdde7e69913
MassimoParisi <massimop98@outlook.it>
2021-11-25 13:50:27 +0100

    2016 day6 problems

---
+++ 2016/Day06/problem1.py
@@ -0,0 +1,23 @@
+words = []
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        words.append(line[:-1])
+
+#for w in words:
+#    print(w)
+res = []
+n = len(words)
+for i in range(8):
+    freq_dict = dict()
+    for j in range(n):
+        curr = words[j][i]
+        if curr in freq_dict:
+            freq_dict[curr] += 1
+        else:
+            freq_dict[curr] = 1
+    max_chr = max(freq_dict,key= freq_dict.get)
+    #print(max_chr)
+    res.append(max_chr)
+
+print("".join(res))
