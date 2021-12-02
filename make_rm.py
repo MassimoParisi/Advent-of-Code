0fde20e0bdfb4a91f7bd42fd85b4a516d53e7683
MassimoParisi <massimop98@outlook.it>
2021-12-02 19:15:58 +0100

    Added README updater

---
+++ make_rm.py
@@ -0,0 +1,24 @@
+import os
+from jinja2 import Template
+
+years= [x for x in range(2015,2022)]
+dirnames = [f"Day{day:02}" for day in range(1, 26)]
+
+file_check = {}
+for year in years:
+    file_check[year] = {}
+    for day in range(1, 26):
+        try:
+            file_check[year][day] = (1 if len(os.listdir(path = f"{year}/Day{day:02}")) > 0 else 0)
+        except:
+            file_check[year][day] = 0
+
+# print(file_check)
+
+with open("templates/readme.html", "r") as f:
+    t = Template(f.read())
+# t = Template("templates/readme.html")
+rendered_template = t.render(years = file_check.keys(), file_check=file_check, dirnames=dirnames)
+
+with open("README.md", "w+") as f:
+    f.write(rendered_template)
