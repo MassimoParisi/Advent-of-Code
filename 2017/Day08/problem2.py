03f2bf15c46b72b63b1fcc79eb3a2ae0001f7060
MassimoParisi <massimop98@outlook.it>
2021-12-08 23:29:06 +0100

    2017 day8 problems

---
+++ 2017/Day08/problem2.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+registers = defaultdict(lambda: 0)
+
+res = 0
+
+cond_dict = {
+    ">": lambda x,y: x > y,
+    ">=": lambda x,y: x >= y,
+    "<": lambda x,y: x < y,
+    "<=": lambda x,y: x <= y,
+    "==": lambda x,y: x == y,
+    "!=": lambda x,y: x != y
+}
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        reg1, op, qty1, _, reg2, cond, qty2 = line[:-1].split(" ")
+        qty1, qty2 = int(qty1), int(qty2)
+        if cond_dict[cond](registers[reg2], qty2):
+            if op == "inc":
+                registers[reg1] += qty1
+                res = max(res,registers[reg1])
+            else:
+                registers[reg1] -= qty1
+
+print(res)
